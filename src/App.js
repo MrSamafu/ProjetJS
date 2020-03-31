5bd48a8cf75816955c8fd5c2ee8ce76e4e88d036
Ryoley <chevrierpaul.pc@gmail.com>
2020-03-31 11:29:26 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,124 +9,122 @@ import {
   Text,
   Heading,
   Button,
-  Link,
-  Image
+  Image,
+  IconButton
 } from '@chakra-ui/core'
-import Profil from './profil.js'
+import Profil from './profil'
 
 const App = () => (
   <ThemeProvider theme={theme}>
     <CSSReset />
-    <Flex
+    <Flex // Header
+      backgroundColor="gray.100"
       display="flex"
-      flexDirection="column"
+      flexDirection="row"
       alignItems="center"
       justifyContent="center"
       textAlign="center"
       overflow="visible"
       width="100%"
-      height="50%"
+      height="20%"
     >
       <Flex
-        display="flex"
-        flexDirection="row"
-        alignItems="center"
         justifyContent="center"
-        width="100%"
-        height="50%"
+        alignItems="center"
+        width="75%"
+        height="100%"
       >
         <Flex
-          display="flex"
-          flexDirection="row"
-          alignItems="center"
-          justifyContent="center"
-          width="100%%"
+          width="100%"
           height="100%"
+          justifyContent="center"
+          alignItems="center"
+          textAlign="center"
         >
           <Flex
-            display="flex"
-            flexDirection="row"
-            alignItems="center"
             justifyContent="center"
-            width="100%"
+            alignItems="center"
+            width="25%"
             height="100%"
-            m={5}
           >
-            <Link href="https://playoverwatch.com/fr-fr/" target="_blank">
-              <Image
-                height="75px"
-                width="75px"
-                src="https://www.stickpng.com/assets/images/586273b931349e0568ad89df.png"
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-              />
-            </Link>
+            <Image // Logo Overwatch
+              width="25%"
+              src="https://www.stickpng.com/assets/images/586273b931349e0568ad89df.png"
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+            />
           </Flex>
-          <Text
-            fontFamily="Bangers"
-            color="orange.300"
-            letterSpacing="widest"
-            fontSize="5xl"
-            width="100%"
-            height="100%"
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-          >
-            Overview
-          </Text>
+          <Flex width="75%" height="100%" p={5}>
+            <Text
+              fontFamily="Bangers"
+              textAlign="center"
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              fontSize="5xl"
+              color="orange.300"
+              width="100%"
+              height="100%"
+            >
+              Overview
+            </Text>
+          </Flex>
         </Flex>
       </Flex>
       <Flex
         flexDirection="row"
         justifyContent="center"
         alignItems="center"
-        border="100px"
-        borderColor="orange.900"
-      >
-        <Profil />
-      </Flex>
-      <Flex
-        flexDirection="row"
-        justifyContent="center"
-        alignItems="center"
-        width="100%"
-        height="50%"
+        width="25%"
+        height="100%"
       >
         <Flex
           justifyContent="center"
           alignItems="center"
+          p={5}
           height="100%"
           width="100%"
+          flexDirection="column"
         >
-          <Button
-            m={5}
-            backgroundColor="orange.300"
-            color="whiteAlpha.900"
-            letterSpacing="widest"
-            width="200px"
-            height="50px"
-            borderRadius="25px"
-            fontSize="lg"
-          >
-            Sign Up
-          </Button>
-          <Button
-            m={5}
+          <IconButton // Bouton pour s'enregistrer
+            m={3}
             backgroundColor="orange.300"
             color="whiteAlpha.900"
             letterSpacing="widest"
             height="50px"
-            width="200px"
-            borderRadius="25px"
-            fontSize="lg"
+            width="25%"
+            borderRadius="10px"
+            fontSize="100%"
             display="flex"
             justifyContent="center"
             alignItems="center"
+            size="md"
+            variantColor="yellow"
+            flexDirection="column"
+            icon="email"
+          >
+            Sign Up
+          </IconButton>
+          <IconButton // Bouton pour se connecter
+            m={3}
+            backgroundColor="orange.300"
+            color="whiteAlpha.900"
+            letterSpacing="widest"
+            height="50px"
+            width="25%"
+            borderRadius="10px"
+            fontSize="100%"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            size="md"
+            variantColor="yellow"
+            flexDirection="column"
+            icon="phone"
           >
             Sign In
-          </Button>
+          </IconButton>
         </Flex>
       </Flex>
     </Flex>
@@ -138,7 +136,7 @@ const App = () => (
       justifyContent="center"
       alignItems="center"
       width="100%"
-      height="100%"
+      height="70%"
     >
       <Box
         display="flex"
@@ -166,14 +164,14 @@ const App = () => (
             alignItems="center"
             justifyContent="center"
             backgroundColor="facebook.300"
-            width="75%"
+            width="85%"
             height="100px"
             borderRadius="25px"
             mt={5}
             mb={5}
             letterSpacing="widest"
           >
-            <Heading
+            <Heading // Titre du contenu
               size="md"
               as="h1"
               lineHeight="shorter"
@@ -186,16 +184,16 @@ const App = () => (
               textAlign="center"
               letterSpacing="widest"
               color="orange.300"
-              fontSize="3xl"
+              fontSize="4xl"
             >
-              Tracer
+              Home
             </Heading>
           </Flex>
           <Flex
             justifyContent="center"
             alignItems="center"
             height="100%"
-            width="100%"
+            width="85%"
             mb={5}
             color="facebook.900"
           >
@@ -205,7 +203,7 @@ const App = () => (
               alignItems="center"
               justifyContent="center"
               backgroundColor="whiteAlpha.900"
-              width="75%"
+              width="100%"
               height="100%"
               borderRadius="25px"
             >
@@ -221,75 +219,48 @@ const App = () => (
                 textAlign="justify"
                 letterSpacing="widest"
                 fontSize="lg"
-                width="75%"
+                width="100%"
                 height="100%"
-                m={5}
               >
-                L’ancien agent d’Overwatch connu sous le pseudonyme de Tracer
-                est une aventurière capable de se déplacer dans le temps et une
-                force irrépressible au service du bien. Lena Oxton (nom de code
-                « Tracer ») était la plus jeune recrue de toute l’histoire du
-                programme de vol expérimental d’Overwatch. Ses talents de pilote
-                et son intrépidité lui valurent d’être sélectionnée pour tester
-                un prototype de chasseur-téléporteur, le Sillage.
-                Malheureusement, la matrice de téléportation de l’avion eut une
-                avarie lors du premier vol d’essai et disparut. Lena fut
-                présumée morte. Elle finit par réapparaître des mois plus tard,
-                profondément changée par l’épreuve qu’elle venait de traverser :
-                sa structure moléculaire avait été désynchronisée par rapport à
-                l’écoulement normal du temps. Souffrant de « chrono-dissociation
-                », elle était devenue une sorte de fantôme vivant, disparaissant
-                irrégulièrement pendant des heures ou des jours et incapable de
-                maintenir sa consistance physique dans les rares moments où elle
-                était présente. Les médecins et les chercheurs d’Overwatch
-                étaient perplexes, et son cas semblait désespéré jusqu’à ce
-                qu’un scientifique appelé Winston parvienne à concevoir le
-                chrono-accélérateur, un appareil capable d’ancrer Tracer dans le
-                présent. Cela permit également à Tracer de contrôler son propre
-                flux temporel et de le ralentir ou de l’accélérer à volonté.
-                Grâce à cette nouvelle compétence, elle devint l’un des agents
-                les plus efficaces d’Overwatch. Depuis la dissolution de
-                l’organisation, Tracer continue de lutter pour la bonne cause et
-                de combattre les injustices dès qu’elle en a l’occasion.
+               <Profil /* Appel le composant "Profil" */ />
               </Heading>
             </Flex>
           </Flex>
         </Box>
       </Box>
     </Grid>
-    <Flex
+    <Flex // Footer
       display="flex"
       flexDirection="row"
       alignItems="center"
       justifyContent="center"
       textAlign="center"
-      backgroundColor="whiteAlpha.900"
+      backgroundColor="gray.100"
       overflow="visible"
       width="100%"
       height="10%"
-      position="scroll"
+      position="fixed"
+      bottom="0"
     >
       <Flex
         display="flex"
         flexDirection="row"
         alignItems="center"
         justifyContent="center"
-        width="100%%"
+        width="25%"
         height="100%"
       >
-        <Link href="https://playoverwatch.com/fr-fr/" target="_blank">
-          <Image
-            height="50px"
-            width="50px"
-            src="https://www.stickpng.com/assets/images/586273b931349e0568ad89df.png"
-            m={5}
-          />
-        </Link>
+        <Image
+          height="50px"
+          width="50px"
+          src="https://www.stickpng.com/assets/images/586273b931349e0568ad89df.png"
+          m={5}
+        />
       </Flex>
       <Text
         letterSpacing="widest"
-        fontSize="lg"
-        width="100%"
+        fontSize="100%"
+        width="75%"
         height="100%"
         display="flex"
         justifyContent="center"
@@ -297,6 +268,21 @@ const App = () => (
       >
         2020 © Overview. All rights reserved.
       </Text>
+      <Flex
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="center"
+        width="25%"
+        height="100%"
+      >
+        <Image
+          height="50px"
+          width="50px"
+          src="https://www.stickpng.com/assets/images/586273b931349e0568ad89df.png"
+          m={5}
+        />
+      </Flex>
     </Flex>
   </ThemeProvider>
 )
--- src/profil.js
+++ src/profil.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   ThemeProvider,
   CSSReset,
@@ -10,7 +10,7 @@ import {
 
 
 class Profil extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       name: "Loading...",
@@ -19,12 +19,12 @@ class Profil extends Component {
       bronze: "Loading...",
       profilImg: "https://www.freepnglogos.com/uploads/vintage-overwatch-logo-png-12.png",
       levelIcon: "https://www.freepnglogos.com/uploads/vintage-overwatch-logo-png-12.png",
-      level : "Loading...",
+      level: "Loading...",
       CharacterName: [],
       CharacterImg: "https://www.freepnglogos.com/uploads/vintage-overwatch-logo-png-12.png",
 
 
-      error:null,
+      error: null,
 
     }
   }
@@ -34,13 +34,15 @@ class Profil extends Component {
       .then(res => res.json())
       .then(
         (result) => {
-          this.setState({name: result.name,
-                        gold: result.quickPlayStats.careerStats.allHeroes.matchAwards.medalsGold,
-                        silver: result.quickPlayStats.careerStats.allHeroes.matchAwards.medalsSilver,
-                        bronze: result.quickPlayStats.careerStats.allHeroes.matchAwards.medalsBronze,
-                        profilImg: result.icon,
-                        levelIcon: result.prestigeIcon,
-                        level: result.level});
+          this.setState({
+            name: result.name,
+            gold: result.quickPlayStats.careerStats.allHeroes.matchAwards.medalsGold,
+            silver: result.quickPlayStats.careerStats.allHeroes.matchAwards.medalsSilver,
+            bronze: result.quickPlayStats.careerStats.allHeroes.matchAwards.medalsBronze,
+            profilImg: result.icon,
+            levelIcon: result.prestigeIcon,
+            level: result.level
+          });
           console.log(result);
         },
         (error) => {
@@ -51,85 +53,85 @@ class Profil extends Component {
         }
       );
   }
-  render(){
+  render() {
     return <ThemeProvider theme={theme}>
-    <CSSReset />
-    <Flex justifyContent="space-around">
-      <Flex justifyContent="flex-end">
-        <Image
-          height="100px"
-          width="100px"
-          borderRadius = "10px"
-          src={this.state.profilImg}
-          display="flex"
-          id="imgPlayer"
-        />
-        <Flex flexDirection="column">
-          <Text id="playerName">{this.state.name}</Text>
-          <Text id="lastlogin">derniere connexion</Text>
-        </Flex>
-      </Flex>
-      <Flex display="flex" justifyContent="flex-start">
-        <Image
-          height="110px"
-          width="110px"
-          src={this.state.levelImg}
-          display="flex"
-          id="imgLvl"
-        />
-        <Text id="lvl">Level : {this.state.level}</Text>
-      </Flex>
-    </Flex>
-    <Flex justifyContent="space-around" alignItems="flex-start">
-      <Flex>
-        <Image
-          height="100px"
-          width="100px"
-          src="https://www.overbuff.com/assets/images/heroes/moira.png?v=eee943d"
-          id="characterImg"
-        />
-        <Flex flexDirection="column">
+      <CSSReset />
+      <Flex justifyContent="space-around">
+        <Flex justifyContent="flex-end">
+          <Image
+            height="100px"
+            width="100px"
+            borderRadius="10px"
+            src={this.state.profilImg}
+            display="flex"
+            id="imgPlayer"
+          />
           <Flex flexDirection="column">
-            <Text id="nameCharacter">Nom du héro</Text>
-            <Text id="lastPlayed">Dernière partie</Text>
+            <Text id="playerName">{this.state.name}</Text>
+            <Text id="lastlogin">derniere connexion</Text>
           </Flex>
-          <Flex>
-            <Text id="quickScore">Score rapide</Text>
-            <Text id="victory">Victoires</Text>
+        </Flex>
+        <Flex display="flex" justifyContent="flex-start">
+          <Image
+            height="110px"
+            width="110px"
+            src={this.state.levelImg}
+            display="flex"
+            id="imgLvl"
+          />
+          <Text id="lvl">Level : {this.state.level}</Text>
+        </Flex>
+      </Flex>
+      <Flex justifyContent="space-around" alignItems="flex-start">
+        <Flex>
+          <Image
+            height="100px"
+            width="100px"
+            src="https://www.overbuff.com/assets/images/heroes/moira.png?v=eee943d"
+            id="characterImg"
+          />
+          <Flex flexDirection="column">
+            <Flex flexDirection="column">
+              <Text id="nameCharacter">Nom du héro</Text>
+              <Text id="lastPlayed">Dernière partie</Text>
+            </Flex>
+            <Flex>
+              <Text id="quickScore">Score rapide</Text>
+              <Text id="victory">Victoires</Text>
+            </Flex>
+          </Flex>
+        </Flex>
+        <Flex>
+          <Flex flexDirection="column">
+            <Image
+              height="100px"
+              width="100px"
+              src="https://d15f34w2p8l1cc.cloudfront.net/overwatch/1b00b8cab530e98c378de2f3e8834d92ee41b4cd7b118179a8ecbccee83c8104.png"
+              id="imgGold"
+            />
+            <Text id="gold">Gold medals : {this.state.gold}</Text>
+          </Flex>
+          <Flex flexDirection="column">
+            <Image
+              height="100px"
+              width="100px"
+              src="https://d15f34w2p8l1cc.cloudfront.net/overwatch/1b00b8cab530e98c378de2f3e8834d92ee41b4cd7b118179a8ecbccee83c8104.png"
+              id="imgSilver"
+            />
+            <Text id="silver">Silver Medals : {this.state.silver}</Text>
+          </Flex>
+          <Flex flexDirection="column">
+            <Image
+              height="100px"
+              width="100px"
+              src="https://d15f34w2p8l1cc.cloudfront.net/overwatch/1b00b8cab530e98c378de2f3e8834d92ee41b4cd7b118179a8ecbccee83c8104.png"
+              id="imgBronze"
+            />
+            <Text id="bronze">Bronze medals : {this.state.bronze}</Text>
           </Flex>
         </Flex>
       </Flex>
-      <Flex>
-        <Flex flexDirection="column">
-          <Image
-            height="100px"
-            width="100px"
-            src="https://d15f34w2p8l1cc.cloudfront.net/overwatch/1b00b8cab530e98c378de2f3e8834d92ee41b4cd7b118179a8ecbccee83c8104.png"
-            id="imgGold"
-          />
-          <Text id="gold">Gold medals : {this.state.gold}</Text>
-        </Flex>
-        <Flex flexDirection="column">
-          <Image
-            height="100px"
-            width="100px"
-            src="https://d15f34w2p8l1cc.cloudfront.net/overwatch/1b00b8cab530e98c378de2f3e8834d92ee41b4cd7b118179a8ecbccee83c8104.png"
-            id="imgSilver"
-          />
-          <Text id="silver">Silver Medals : {this.state.silver}</Text>
-        </Flex>
-        <Flex flexDirection="column">
-          <Image
-            height="100px"
-            width="100px"
-            src="https://d15f34w2p8l1cc.cloudfront.net/overwatch/1b00b8cab530e98c378de2f3e8834d92ee41b4cd7b118179a8ecbccee83c8104.png"
-            id="imgBronze"
-          />
-          <Text id="bronze">Bronze medals : {this.state.bronze}</Text>
-        </Flex>
-      </Flex>
-    </Flex>
-  </ThemeProvider>
+    </ThemeProvider>
   }
 }
 
