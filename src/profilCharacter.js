ee9430ec6311eb39a6f4bf3a557554715544458c
Samson <51507673+MrSamafu@users.noreply.github.com>
2020-03-31 14:10:10 +0200

    ImgCharacter in Profil

--- src/profilCharacter.js
+++ src/profilCharacter.js
@@ -7,6 +7,7 @@ import {
   Image,
   Flex
 } from '@chakra-ui/core'
+import Donnee from "./ressources/ImgCharacter.js"
 
 class ProfilCharacter extends Component {
   constructor(props) {
@@ -48,39 +49,39 @@ class ProfilCharacter extends Component {
     let tableauxDeDonnee = Object.keys(this.state.name);
     let screen = [];
     let count = -1;
+    let picture;
     for (let nameCharacter of tableauxDeDonnee) {
-      console.log(this.state.name[nameCharacter])
-
-      screen[count] = 
-      <Flex>
-      <Flex>
-        <Image
-          height="100px"
-          width="100px"
-          src="https://d15f34w2p8l1cc.cloudfront.net/overwatch/1b00b8cab530e98c378de2f3e8834d92ee41b4cd7b118179a8ecbccee83c8104.png"
-        />
-        <Flex flexDirection="column">
-          <Text>Nom du héros</Text>
-          <Text>Temps de jeu : {this.state.name[nameCharacter]["game"]["timePlayed"]}</Text>
+      picture = Donnee(count);
+      screen[count] =
+        <Flex>
           <Flex>
-            <Text>Meilleur score Dommage : {this.state.name[nameCharacter]["best"]["allDamageDoneMostInGame"]}</Text>
-            <Text>Victoires : {this.state.name[nameCharacter]["game"]["gamesWon"]}</Text>
+            <Image
+              height="150px"
+              width="100px"
+              src={picture}
+            />
+            <Flex flexDirection="column">
+              <Text>Nom du héros</Text>
+              <Text>Temps de jeu : {this.state.name[nameCharacter]["game"]["timePlayed"]}</Text>
+              <Flex>
+                <Text>Meilleur score Dommage : {this.state.name[nameCharacter]["best"]["allDamageDoneMostInGame"]}</Text>
+                <Text>Victoires : {this.state.name[nameCharacter]["game"]["gamesWon"]}</Text>
+              </Flex>
+            </Flex>
           </Flex>
         </Flex>
-      </Flex>
-    </Flex>
       count++;
     }
     const listItems = screen.map((characterStats) =>
       <div>{characterStats}</div>);
-              
-              
-            
-      return <Flex flexDirection="column" alignItems="">
-      
+
+
+
+    return <Flex flexDirection="column" alignItems="">
+
       {listItems}
     </Flex>
-   
+
 
   }
 }
