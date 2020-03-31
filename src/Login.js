9d4c036062c2de702cda185c6ca779ea59689430
nathanlecat <nathan.lecat@viacesi.fr>
2020-03-31 15:57:38 +0200

    login

--- src/Login.js
+++ src/Login.js
@@ -1,95 +1,48 @@
-import React from 'react'
-import {
-    ThemeProvider,
-    CSSReset,
-    theme,
-    Icon,
-    Heading,
-    InputGroup,
-    InputLeftAddon,
-    Input,
-    InputRightElement,
-    Checkbox,
-    Button
-} from '@chakra-ui/core'
+import React from 'react';
+import fire from './config/fire';
 
-const App = () => (
-    <ThemeProvider theme={theme}>
-        <CSSReset />
-        <Icon name="copy" fontSize="4xl" backgroundColor="whiteAlpha.500" />
-        <Heading
-            as="h1"
-            color="yellow.500"
-            letterSpacing="wider"
-            lineHeight="taller"
-            textAlign="center"
-        >
-            Connexion
-    </Heading>
-        <InputGroup />
-        <InputGroup
-            mt={10}
-            fontStyle="italic"
-            color="yellow.500"
-            backgroundColor="yellow.900"
-        >
-            <InputLeftAddon>Email: </InputLeftAddon>
-            <Input placeholder="Entrez votre email !" letterSpacing="widest" />
-            <InputRightElement>
-                <Icon name="email" />
-            </InputRightElement>
-        </InputGroup>
-        <InputGroup
-            mt={10}
-            textAlign="left"
-            fontStyle="italic"
-            letterSpacing="wide"
-            backgroundColor="gray.900"
-        >
-            <InputLeftAddon color="yellow.500">Password:</InputLeftAddon>
-            <Input
-                isFullWidth
-                placeholder="Entrez votre mot de passe !"
-                variant="outline"
-                opacity={1}
-                letterSpacing="widest"
-                lineHeight="tall"
-            />
-            <InputRightElement>
-                <Icon name="password" color="yellow.500" />
-            </InputRightElement>
-        </InputGroup>
-        <Checkbox
-            isReadOnly
-            variantColor="yellow"
-            color="yellow.500"
-            fontStyle="italic"
-            textAlign="center"
-            height={10}
-            display="flex"
-            flexDirection="row"
-            justifyContent="center"
-            mt={10}
-        >
-            Vérification..
-    </Checkbox>
-        <Button
-            fontStyle="italic"
-            textAlign="justify"
-            color="yellow.500"
-            mt={10}
-            display="flex"
-            rightIcon="arrow-forward"
-            justifyContent="center"
-            borderRadius={10}
-            flexDirection="row"
-            alignItems="stretch"
-            pr={2}
-            variant="solid"
-        >
-            Connexion
-    </Button>
-    </ThemeProvider>
-)
+class Login extends React.Component {
 
-export default App
+    signUp() {
+        const email = document.querySelector('#email').value;
+        const password = document.querySelector('#password').value;
+        fire.auth().createUserWithEmailAndPassword(email, password)
+            .then((u) => {
+                console.log('Successfully Signed Up');
+            })
+            .catch((err) => {
+                console.log('Error: ' + err.toString());
+            })
+    }
+
+    login() {
+        const email = document.querySelector('#email').value;
+        const password = document.querySelector('#password').value;
+        fire.auth().signInWithEmailAndPassword(email, password)
+            .then((u) => {
+                console.log('Successfully Logged In');
+            })
+            .catch((err) => {
+                console.log('Error: ' + err.toString());
+            })
+    }
+
+    render() {
+        return (
+            <div style={{ textAlign: 'center' }}>
+                <div>
+                    <div>Email</div>
+                    <input id="email" placeholder="Enter Email.." type="text"/>
+                </div>
+                <div>
+                    <div>Password</div>
+                    <input id="password" placeholder="Enter Password.." type="text"/>
+                </div>
+                <button style={{margin: '10px'}} onClick={this.login}>Login</button>
+                <button style={{margin: '10px'}} onClick={this.signUp}>Sign Up</button>
+            </div>
+        )
+    }
+}
+
+export default Login;
