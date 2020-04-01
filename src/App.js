a82fd981df05da10eb8a868b5831a7cb1e9502ed
Samson <51507673+MrSamafu@users.noreply.github.com>
2020-04-01 17:13:21 +0200

    modification de profileChatracter

--- src/App.js
+++ src/App.js
@@ -9,20 +9,18 @@ import Home from './Home'
 import Login from './Login'
 import SignUp from './SignUp'
 import PrivateRoute from "./PrivateRoute";
-import {AuthProvider} from "./Auth";
+
 
 const App = () => (
   <ThemeProvider theme={theme}>
     <CSSReset />
-    <AuthProvider>
     <Router>
       <Switch>
-        <PrivateRoute path="/" exact component={Home} />
+        <Route path="/" exact component={Home} />
         <Route path="/register" component={SignUp} />
         <Route path="/login" component={Login} />
       </Switch>
     </Router>
-    </AuthProvider>
   </ThemeProvider>
 )
 
