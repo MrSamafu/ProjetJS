d4e5b2178a3b3f6313e224b79f322fcab7b2c8a4
Samson <51507673+MrSamafu@users.noreply.github.com>
2020-04-03 02:08:10 +0200

    front SignUp

--- src/firebase/SignUp.js
+++ src/firebase/SignUp.js
@@ -5,19 +5,17 @@ import {
   Button,
   Flex,
   Image,
-  Popover,
-  PopoverArrow, PopoverBody, PopoverCloseButton,
-  PopoverContent, PopoverHeader,
-  PopoverTrigger,
-  Select,
-  Text
+  Text,
+  FormLabel,
+  Input,
+  Select
 } from "@chakra-ui/core";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const SignUp = ({ history }) => {
   const handleSignUp = useCallback(async event => {
     event.preventDefault();
-    const { email, password,BattleNet,PlateForm} = event.target.elements;
+    const { email, password, BattleNet, PlateForm } = event.target.elements;
 
     try {
       await config
@@ -31,7 +29,7 @@ const SignUp = ({ history }) => {
       });
 
       config.database().ref('plate/' + letUserId).set({
-      PlateForm: PlateForm.value,
+        PlateForm: PlateForm.value,
       });
       alert("Account is created");
       history.push("/");
@@ -43,54 +41,54 @@ const SignUp = ({ history }) => {
   return (
     <div>
       <Flex // Header
-          backgroundColor="gray.100"
-          display="flex"
-          flexDirection="row"
-          alignItems="center"
-          justifyContent="center"
-          textAlign="center"
-          overflow="visible"
-          width="100%"
-          height="20%"
+        backgroundColor="gray.100"
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="center"
+        textAlign="center"
+        overflow="visible"
+        width="100%"
+        height="20%"
       >
         <Flex
-            justifyContent="center"
-            alignItems="center"
-            width="75%"
-            height="100%"
+          justifyContent="center"
+          alignItems="center"
+          width="75%"
+          height="100%"
         >
           <Flex
-              width="100%"
-              height="100%"
-              justifyContent="center"
-              alignItems="center"
-              textAlign="center"
+            width="100%"
+            height="100%"
+            justifyContent="center"
+            alignItems="center"
+            textAlign="center"
           >
             <Flex
-                justifyContent="center"
-                alignItems="center"
-                width="25%"
-                height="100%"
+              justifyContent="center"
+              alignItems="center"
+              width="25%"
+              height="100%"
             >
               <Image // Logo Overwatch
-                  width="25%"
-                  src="https://www.stickpng.com/assets/images/586273b931349e0568ad89df.png"
-                  display="flex"
-                  justifyContent="center"
-                  alignItems="center"
+                width="25%"
+                src="https://www.stickpng.com/assets/images/586273b931349e0568ad89df.png"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
               />
             </Flex>
             <Flex width="75%" height="100%" p={5}>
               <Text
-                  fontFamily="Bangers"
-                  textAlign="center"
-                  display="flex"
-                  justifyContent="center"
-                  alignItems="center"
-                  fontSize="5xl"
-                  color="orange.300"
-                  width="100%"
-                  height="100%"
+                fontFamily="Bangers"
+                textAlign="center"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                fontSize="5xl"
+                color="orange.300"
+                width="100%"
+                height="100%"
               >
                 Overview
               </Text>
@@ -98,70 +96,70 @@ const SignUp = ({ history }) => {
           </Flex>
         </Flex>
         <Flex
-            flexDirection="row"
-            justifyContent="center"
-            alignItems="center"
-            width="25%"
-            height="100%"
-            m={5}
+          flexDirection="row"
+          justifyContent="center"
+          alignItems="center"
+          width="25%"
+          height="100%"
+          m={5}
         >
 
 
           <Flex
-              justifyContent="center"
-              alignItems="center"
-              p={5}
-              height="100%"
-              width="140px"
-              flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            p={5}
+            height="100%"
+            width="140px"
+            flexDirection="column"
           >
 
             <Link to="/">
-                  <Button
-                      m={3}
-                      backgroundColor="orange.300"
-                      color="whiteAlpha.900"
-                      letterSpacing="widest"
-                      height="50px"
-                      width="100%"
-                      borderRadius="10px"
-                      fontSize="100%"
-                      display="flex"
-                      justifyContent="center"
-                      alignItems="center"
-                      size="md"
-                      variantColor="yellow"
-                      flexDirection="column"
-                      textAlign="center"
+              <Button
+                m={3}
+                backgroundColor="orange.300"
+                color="whiteAlpha.900"
+                letterSpacing="widest"
+                height="50px"
+                width="100%"
+                borderRadius="10px"
+                fontSize="100%"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                size="md"
+                variantColor="yellow"
+                flexDirection="column"
+                textAlign="center"
 
-                  >
+              >
 
-                    Home
+                Home
                   </Button>
-                </Link>
+            </Link>
             <Flex flexDirection="column">
               <Flex>
                 <Image
-                    width="25%"
-                    src="https://s18955.pcdn.co/wp-content/themes/sharethis-custom/assets/images/facebookimg.png"
-                    display="flex"
-                    justifyContent="center"
-                    alignItems="center"
-                    mr={1}
+                  width="25%"
+                  src="https://s18955.pcdn.co/wp-content/themes/sharethis-custom/assets/images/facebookimg.png"
+                  display="flex"
+                  justifyContent="center"
+                  alignItems="center"
+                  mr={1}
                 >
                 </Image>
                 <div class="fb-share-button" data-href="http://localhost:3000/" data-layout="button_count" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Partager</a></div>
               </Flex>
               <Flex
-                  mt={1}
+                mt={1}
               >
                 <Image
-                    width="25%"
-                    src="https://s18955.pcdn.co/wp-content/themes/sharethis-custom/assets/images/twitterimg.png"
-                    display="flex"
-                    justifyContent="center"
-                    alignItems="center"
-                    mr={1}
+                  width="25%"
+                  src="https://s18955.pcdn.co/wp-content/themes/sharethis-custom/assets/images/twitterimg.png"
+                  display="flex"
+                  justifyContent="center"
+                  alignItems="center"
+                  mr={1}
                 >
                 </Image>
                 <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="true">Partager</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
@@ -170,77 +168,132 @@ const SignUp = ({ history }) => {
           </Flex>
         </Flex>
       </Flex>
-      <Flex width="75%" height="100%" p={5}>
-        <Text
-            fontFamily="Bangers"
-            textAlign="center"
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            fontSize="5xl"
-            color="orange.300"
-            width="150%"
-            height="100%"
+      <Flex
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+
+
+      >
+        <Flex
+          flexDirection="column"
+          backgroundColor="gray.100"
+          borderRadius="15px"
+          mt='5%'
+          w="50%"
+          pb="15px"
+
         >
-         _Sign-Up_
-        </Text>
+          <Flex height="100%" justifyContent="center">
+            <Text
+              fontFamily="Bangers"
+              textAlign="center"
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              fontSize="5xl"
+              color="orange.300"
+              height="100%"
+            >
+              _Sign-Up_
+                        </Text>
+          </Flex>
+          <form onSubmit={handleSignUp}>
+            <Flex
+              flexDirection="column"
+              justifyContent="center"
+              size="90%"
+
+            >
+              <Flex
+                flexDirection="column"
+                gap={1}
+                ml="10%"
+              >
+                <FormLabel
+                  textAlign="center"
+                >
+                  Pseudo
+                                </FormLabel>
+                <Input name="email" type="email" placeholder="Pseudo" borderRadius="15px" textAlign="center" />
+              </Flex>
+              <Flex
+                flexDirection="column"
+
+                gap={1}
+                ml="10%"
+              >
+                <FormLabel
+                  textAlign="center"
+                >
+                  Choose your plateform
+                                </FormLabel>
+                <Select
+                  name="PlateForm"
+                  id="platformSelection"
+                  placeholder="Platforms"
+                  w="100%"
+                  letterSpacing="wide"
+                >
+                  <option value="pc">PC</option>
+                  <option value="xbl">XBOX</option>
+                  <option value="psn">PS4</option>
+                  <option value="nintendo-switch">Nintendo Switch</option>
+                </Select>
+              </Flex>
+              <Flex
+                flexDirection="column"
+                gap={1}
+                ml="10%"
+              >
+                <FormLabel
+                  textAlign="center"
+                >
+                  Email
+                                </FormLabel>
+                <Input name="email" type="email" placeholder="Email" borderRadius="15px" textAlign="center" />
+              </Flex>
+              <Flex
+                flexDirection="column"
+                justifyContent="center"
+                gap={1}
+                ml="10%"
+              >
+                <FormLabel
+                  textAlign="center"
+                >
+                  Password
+                                </FormLabel>
+                <Input name="password" type="password" placeholder="Password" borderRadius="15px" textAlign="center" />
+              </Flex>
+              <Flex
+                justifyContent="center"
+              >
+                <Button
+                  mt="15px"
+                  backgroundColor="orange.300"
+                  color="whiteAlpha.900"
+                  letterSpacing="widest"
+                  height="50px"
+                  width ="125px"
+                  borderRadius="10px"
+                  fontSize="100%"
+                  variantColor="yellow"
+                  textAlign="center"
+                  type="submit"
+
+
+                >
+
+                  Sign Up
+                            </Button>
+              </Flex>
+
+            </Flex>
+
+          </form>
+        </Flex>
       </Flex>
-      <h1 ></h1>
-      <form onSubmit={handleSignUp}>
-        <label>
-        Pseudo :
-        <input justifyContent="center" name="BattleNet" type="text" placeholder="Peudo Battle.net" />
-      </label>
-        <label>
-
-          <Select
-              name="PlateForm"
-              id="platformSelection"
-              placeholder="Platforms"
-              w="50%"
-              letterSpacing="wide"
-          >
-            <option value="pc">PC</option>
-            <option value="xbl">XBOX</option>
-            <option value="psn">PS4</option>
-            <option value="nintendo-switch">Nintendo Switch</option>
-          </Select>
-        </label>
-
-        <label>
-          Email :
-          <input name="email" type="email" placeholder="Email" />
-        </label>
-        <label>
-          Password :
-          <input name="password" type="password" placeholder="Password" />
-        </label>
-        <br />
-        <br />
-        <br />
-        <Button
-            m={3}
-            backgroundColor="orange.300"
-            color="whiteAlpha.900"
-            letterSpacing="widest"
-            height="50px"
-            width="100%"
-            borderRadius="10px"
-            fontSize="100%"
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            size="md"
-            variantColor="yellow"
-            flexDirection="column"
-            textAlign="center"
-            type="submit"
-
-        >
-
-          Sign Up
-        </Button>
-      </form>
     </div>
   );
 };
