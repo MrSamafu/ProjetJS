e9ca5deb6ca1782ae5b784227d7058da3a44b6c5
Samson <51507673+MrSamafu@users.noreply.github.com>
2020-04-03 02:43:47 +0200

    Update Home.js

--- src/interface/Home.js
+++ src/interface/Home.js
@@ -302,7 +302,7 @@ class Home extends Component {
                                         mr={1}
                                     >
                                     </Image>
-                                    <div class="fb-share-button" data-href="http://localhost:3000/" data-layout="button_count" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Partager</a></div>                    
+                                    <div class="fb-share-button" data-href="http://localhost:3000/" data-layout="button_count" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>                    
                                 </Flex>
                                 <Flex
                                     mt={1}
@@ -316,7 +316,7 @@ class Home extends Component {
                                         mr={1}
                                     >
                                     </Image>
-                                <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="true">Partager</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                                <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="true">Share</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
                                 </Flex>
                             </Flex>                            
                             </Flex>
